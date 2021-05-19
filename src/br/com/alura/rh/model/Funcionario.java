1d9151de519efd23726fe16a992728c1a749cd63
luis.neto <luis.neto@cidadania.gov.br>
2021-05-19 08:43:49 -0300

    Initial commit

---
+++ src/br/com/alura/rh/model/Funcionario.java
@@ -0,0 +1,59 @@
+package br.com.alura.rh.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario {
+
+	private DadosPessoais dadosPessoais;
+	private Cargo cargo;
+	private BigDecimal salario;
+	private LocalDate dataUltimoReajuste;
+
+	public Funcionario(DadosPessoais dadosPessoais, Cargo cargo, BigDecimal salario) {
+		this.dadosPessoais = dadosPessoais;
+		this.cargo = cargo;
+		this.salario = salario;
+	}
+
+	public void atualizarSalario(BigDecimal novoSalario) {
+		this.salario = novoSalario;
+		this.dataUltimoReajuste = LocalDate.now();
+	}
+
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
+	}
+
+	public void setDadosPessoais(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
+	}
+
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public LocalDate getDataUltimoReajuste() {
+		return dataUltimoReajuste;
+	}
+
+	public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
+		this.dataUltimoReajuste = dataUltimoReajuste;
+	}
+
+	public void promover(Cargo novoCargo) {
+		this.cargo = novoCargo;
+	}
+}
